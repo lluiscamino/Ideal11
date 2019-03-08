89bf621cb6550dae0aecc9569ed7907a4c2b2d35
lluis <caminoperez.lluis@gmail.com>
2019-03-08 22:03:46 +0100

    Error response when the API cannot connect to the database

--- api/config/db_connection.php
+++ api/config/db_connection.php
@@ -1,4 +1,9 @@
 <?php
 $credentials = json_decode(file_get_contents(ROOT . '/db_credentials.json'), true);
-$mysqli = new mysqli($credentials['server'], $credentials['user'], $credentials['password'], $credentials['db']);
+@$mysqli = new mysqli($credentials['server'], $credentials['user'], $credentials['password'], $credentials['db']);
+if ($mysqli->connect_error) {
+    http_response_code(400);
+    echo json_encode(array('message' => 'Database connection error.'));
+    die();
+}
 unset($credentials);
